442560609a1c61fede2336757db95ca0924490be
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-03-23 15:58:50 -0700

    modified some if else return logic

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -16,6 +16,8 @@ class Queue:
         if self.size > 0:
             self.size -= 1
             return self.storage.remove_from_head()
+        else:
+            return None
 
     def len(self):
         return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -10,11 +10,14 @@ class Stack:
     def push(self, value):
         self.storage.add_to_head(value)
         self.size += 1
+        return value
 
     def pop(self):
         if self.size > 0:
             self.size -= 1
             return self.storage.remove_from_head()
+        else:
+            return None
 
     def len(self):
         return self.size
